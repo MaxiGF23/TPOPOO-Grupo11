02ee0f5a6ed0eb63b6072d54fdbb7c8c963600f9
MaxiGF23 <grecomaxi3@gmail.com>
2024-10-22 19:02:46 -0300

    Limpieza de main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 import Aeronaves.*;
 
-/*
-Se trata de un sistema de gestión de empleados para una fábrica de aeronaves. La fábrica cuenta con dos líneas de producción:
- aeronaves de alas rotativas (helicópteros) y aeronaves de alas fijas (aviones).
-
-Cada línea de producción está destinada a dos mercados distintos: el mercado civil y el mercado militar.
-
-
-El desafío radica en la asignación de empleados según sus responsabilidades, para minimizar los riesgos de filtración de componentes de
-uso militar avanzado o planos de aeronaves civiles. Además, el sistema debe permitir la reasignación de empleados entre las líneas de producción cuando
-sea necesario. Este cambio de línea de producción debe realizarse bajo la autorización de un supervisor, quien ingresará con las credenciales adecuadas.
- */
 public class Main {
     public static void main(String[] args) {
        AeronaveCivilAlaFija Cessna = new AeronaveCivilAlaFija("Cessna P10", "LV050", 1200, 3, 300);
